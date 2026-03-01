5478f713f858130b743f43c165d6b73efd6b872e
LikeAboss32 <upretikaran7@gmail.com>
2026-03-01 21:08:02 +0530

    firebase

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,11 +3,11 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interviewiq-ba6ba.firebaseapp.com",
-  projectId: "interviewiq-ba6ba",
-  storageBucket: "interviewiq-ba6ba.firebasestorage.app",
-  messagingSenderId: "862159592601",
-  appId: "1:862159592601:web:7308d702cd708076ddec08"
+  authDomain: "project-5540e.firebaseapp.com",
+  projectId: "project-5540e",
+  storageBucket: "project-5540e.firebasestorage.app",
+  messagingSenderId: "774947920974",
+  appId: "1:774947920974:web:c7f16c54eee15fefc3771f"
 };
 
 const app = initializeApp(firebaseConfig);
